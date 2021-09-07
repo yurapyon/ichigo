357b91eb2b427b13dbcaf0e0fe94d1e8053e46fa
extrasharp <genericpb@gmail.com>
2021-09-07 01:09:23 -0500

    hello world

---
+++ pages/index.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from 'next';
+import { GetStaticProps } from "next"
+import prisma from '../lib/prisma.ts';
+
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const feed = await prisma.message.findMany({
+    include: {
+      user: {
+        select: { name: true },
+      },
+    },
+  });
+  return { props: { feed } };
+};
+
+function Post(props) {
+  return (
+    <p>
+      { props.content }
+    </p>
+  );
+}
+
+function PostView(props) {
+  return (
+    <>
+    <h1> { props.feed[0].user.name } </h1>
+    { props.feed.map(Post) }
+    </>
+  );
+}
+
+const Home: NextPage = ({ feed }) => {
+  return (
+      <>
+      <Head> </Head>
+      <PostView feed={feed}/>
+      </>
+  )
+}
+
+export default Home
---
+++ pages/settings.tsx
@@ -0,0 +1,99 @@
+import type { NextPage } from 'next';
+import React, { useState } from 'react';
+
+const defaultSettings = {
+  a: false,
+  b: true,
+  c: false,
+  num1: 10,
+  num2: 0,
+};
+
+const Button: React.FC = (props) => {
+  return (
+    <>
+    <input
+      type="checkbox"
+      onChange={props.onChange}
+      checked={props.value}
+      />
+    {props.value ? props.name.toUpperCase() : props.name }
+    </>
+  );
+}
+
+const Slider: React.FC = (props) =>{
+  return (
+    <>
+    <input type="number" onChange={props.onChange} value={props.value} />
+    {props.value}
+    </>
+  );
+}
+
+class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      settings: {...defaultSettings},
+    };
+  }
+
+  handleChange(val, name) {
+    var s = {...this.state.settings};
+    s[name] = val;
+    this.setState({ settings: s });
+  }
+
+  renderSlider(name) {
+    return (
+      <Slider
+        name={name}
+        value={this.state.settings[name]}
+        onChange={(e)=>this.handleChange(e.target.value, name)}
+      />
+    );
+  }
+
+  renderButton(name) {
+    return (
+      <Button
+        name={name}
+        value={this.state.settings[name]}
+        onChange={(e)=>{console.log(e); this.handleChange(e.target.checked, name)}}
+      />
+    );
+  }
+
+  submit() {
+    console.log("submitting: ", this.state.settings);
+  }
+
+  render() {
+    return (
+    <>
+      <p> {this.renderButton("a")} </p>
+      <p> {this.renderButton("b")} </p>
+      <p> {this.renderButton("c")} </p>
+      <p> {this.renderSlider("num1")} </p>
+      <p> {this.renderSlider("num2")} </p>
+      <p> <input
+        type="button"
+        value="submit"
+        onClick={()=>this.submit()}
+        /> </p>
+    </>
+  );
+  }
+}
+
+/*
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // find settings by user
+  const settings = await prisma.settings.findUnique({
+  });
+  return { props: { settings } };
+}
+*/
+
+export default Settings
